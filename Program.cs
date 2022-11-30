907c0a735dd5545a90b1626a8c044de2ddfefa96
Алексей Урсаев <rom-anti-k@yandex.ru>
2022-11-30 09:39:38 +0500

    Added OriginalArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿string[] OriginalArray()
+{
+    Console.WriteLine("Input the number of elements: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string value = string.Empty;
+
+    string[] originalArray = new string[size];
+    
+        for(int i = 0; i < size; i++)
+        {
+            while(value == String.Empty || value == null)
+            {
+                Console.WriteLine($"Input an element {i + 1}: ");
+                value = Console.ReadLine();
+            }
+            originalArray[i] = value;
+        }
+    return originalArray;
+}
+
+
